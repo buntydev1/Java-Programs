d0a03ffe933e5da97b1c05fc3b7dedd831cf4038
yasin <buntyvirani82@gmail.com>
2020-12-10 22:27:59 -0800

    class updated

--- constructor/demo.java
+++ constructor/demo.java
@@ -1,12 +1,13 @@
 import java.io.*;
- class abc
- {
-     int a,b,c;
-     abc()
-    {
+
+class demo {
+    int a, b, c;
+
+    demo() {
         System.out.println("This is Default constructor");
     }
-    abc(int p, int q)
+
+    demo(int p, int q)
     {
         System.out.println("This is Parameterize constructor");
         p = a ;
@@ -14,20 +15,19 @@ import java.io.*;
         System.out.println("this is value of a " + p);
         System.out.println("this is value of b = " q);
     }
-    void disp()
-    {
+
+    void disp() {
         c = a + b;
         System.out.println("Addition of two numbers is " + c);
     }
- }
- class cons_demo
- {
-     public static void main(String [] args)
-     {
-         int a = Integer.parseInt(args[0]);
-         int b = Integer.parseInt(args[1]);
-         abc x = new abc();
-         abc y = new abc(a,b);
-         y.disp();
-     }
- }
+}
+
+class cons_demo {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        demo x = new demo();
+        demo y = new demo(a, b);
+        y.disp();
+    }
+}
