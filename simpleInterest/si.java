9a086489fa65f76e4df94d84340082882ab00a3d
yasin <buntyvirani82@gmail.com>
2020-12-10 22:14:01 -0800

    find simple interest

---
+++ simpleInterest/si.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class interest {
+    void si(int p, int r, int n) {
+        int s = p * r * n / 100;
+        System.out.println("Simple interest is " + s);
+    }
+
+    void disp() {
+        System.out.println("Demo example of si ");
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        interest x = new interest();
+        x.disp();
+        x.si(10000, 10, 2);
+    }
+}
