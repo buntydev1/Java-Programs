8de2165e8ccb44efa4d05c1839b76ca438b31acd
yasin <buntyvirani82@gmail.com>
2020-12-11 03:02:25 -0800

    pie using applet

---
+++ applet/ap9.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+/*<applet code="ap9.class" height="500" width="500">
+</applet>
+*/
+public class ap9 extends Applet
+{
+public void paint(Graphics p)
+{
+p.setColor(Color.red);
+p.fillArc(100,100,400,400,0,-270);
+}
+}
