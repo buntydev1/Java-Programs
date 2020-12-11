d570f1fcedf4ad46563a723f6e229e0618d2a7ef
yasin <buntyvirani82@gmail.com>
2020-12-10 21:58:39 -0800

    addition of two number using class

---
+++ addition/ad.java
@@ -0,0 +1,17 @@
+
+class Add {
+    void disp() {
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = a + b;
+        System.out.println("Addtion of two numbers is " + c);
+    }
+}
+
+class Addition extends Add {
+    public static void main(String[] args) {
+        Add x = new Add();
+        x.disp();
+    }
+}
