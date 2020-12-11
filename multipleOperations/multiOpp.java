b43dc1809b42befb609be6f323617de23fc9fa1b
yasin <buntyvirani82@gmail.com>
2020-12-10 22:49:09 -0800

    multiple operation using package

---
+++ multipleOperations/multiOpp.java
@@ -0,0 +1,22 @@
+import Yasin.*;
+import java.io.*;
+
+class multiOpp {
+    public static void main(String[] args) {
+        check x = new check();
+        x.max(20, 10);
+        x.min(20, 10);
+        x.possitive(20);
+        x.add(10, 20);
+        x.sub(20, 10);
+        x.mul(5, 5);
+        x.div(25, 5);
+        x.odd(24);
+        x.perfect(8);
+        x.swap(20, 30);
+        x.palindrome(5);
+        x.armstrong(4);
+        x.prime(30);
+        x.factor(25);
+    }
+}
